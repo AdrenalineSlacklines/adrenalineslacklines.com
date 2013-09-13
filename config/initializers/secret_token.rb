57b32e0ac4058873733c2abc88edbb9c5c508648
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2013-09-12 19:53:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdrenalineslacklinesCom::Application.config.secret_key_base = '0033b2d384582645997bf18a2e12b5d5f6c8cc35c35778890631b57cda5395c1615ed882920589c9f183dd777dd316e89908a169eaed613d07cff67cc261fa59'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdrenalineslacklinesCom::Application.config.session_store :cookie_store, key: '_adrenalineslacklines_com_session'
