26296d9b8975c43f1ef5cb0963c0d8268c1ea029
Ivan Rodriguez <ivan.rodriguez001@gmail.com>
2013-10-05 14:12:05 -0700

    Add tests for page titles

---
+++ spec/requests/content_pages_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe "ContentPages" do
+  let(:base_title) { "Adrenaline Slacklines" }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_title("#{base_title} | Home") }
+  end
+
+  describe "Athletes page" do
+    before { visit athletes_path }
+
+    it { should have_title("#{base_title} | Athletes") }
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    it { should have_title("#{base_title} | About") }
+  end
+
+  describe "About Sport page" do
+    before { visit about_sport_path }
+
+    it { should have_title("#{base_title} | About the Sport") }
+  end
+
+  describe "Disclaimer page" do
+    before { visit disclaimer_path }
+
+    it { should have_title("#{base_title} | Disclaimer")}
+  end
+
+
+end
